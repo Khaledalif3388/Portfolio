02355d3c41c15a7de290ade3e5433a4b1bedcc41
Md. Khaled Hasan (Alif) <98605014+Khaledalif3388@users.noreply.github.com>
2024-04-25 16:08:09 +0600

    Build with Flutter and dart

---
+++ lib/utils/about_utils.dart
@@ -0,0 +1,7 @@
+class AboutUtils {
+  static const String aboutMeHeadline =
+      'I\'m Khaled Hasan, a UI/UX Designer,  and Frontend Developer.';
+
+  static const String aboutMeDetail =
+      'I\'m pursuing a bachelor degree in computer science from Daffodil International University, Dhaka. I have been designing mobile apps and web interfaces for over 1 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Currently working as Jr. UI/UX Designer at defole.com';
+}
